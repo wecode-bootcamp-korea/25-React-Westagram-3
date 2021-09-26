dbbc28e98ec01233188054069206e8d81418fba8
kangdan <kangdanne@gmail.com>
2021-09-26 21:27:12 +0900

    Add: Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/dan/Login/Login.js
+++ src/pages/dan/Login/Login.js
@@ -13,24 +13,58 @@ class Login extends React.Component {
 
   //ID state
   handleIdInput = event => {
-    this.setState({
-      idValue: event.target.value,
-    });
+    this.setState(
+      {
+        idValue: event.target.value,
+      },
+      () => {
+        this.checkIdValid();
+      }
+    );
   };
 
   // PW state
   handlePwInput = event => {
-    this.setState({
-      pwValue: event.target.value,
-    });
+    this.setState(
+      {
+        pwValue: event.target.value,
+      },
+      () => {
+        this.checkPwValid();
+      }
+    );
   };
 
-  // Main페이지로 이동
+  // ID validation
+  checkIdValid() {
+    const { idValue } = this.state;
+    return idValue.includes('@') && idValue.length > 0 ? true : false;
+  }
+
+  // PW validation
+  checkPwValid() {
+    const { pwValue } = this.state;
+    return pwValue.length >= 5 ? true : false;
+  }
+
+  // Login Button on : off
+  handleButton = () => {
+    this.setState(() => {
+      this.checkIdValid();
+      this.checkPwValid();
+    });
+    return this.checkIdValid() === true && this.checkPwValid() === true
+      ? true
+      : false;
+  };
+
+  // Main 페이지로 이동
   goToDanMain = () => {
     this.props.history.push('/Main-D');
   };
 
   render() {
+    const isValid = this.handleButton();
     return (
       <div className="login_container">
         <main className="login">
@@ -40,18 +74,26 @@ class Login extends React.Component {
             <input
               id="userId"
               type="text"
+              name="id"
               placeholder="전화번호, 사용자 이름 또는 이메일"
               value={this.state.idValue}
               onChange={this.handleIdInput}
             />
             <input
               id="userPw"
+              name="pw"
               type="password"
               placeholder="비밀번호"
               value={this.state.pwValue}
               onChange={this.handlePwInput}
             />
-            <button id="btnLogin" type="submit" onClick={this.goToDanMain}>
+            <button
+              id="btnLogin"
+              type="submit"
+              className={isValid ? 'active' : ' '}
+              disabled={!isValid}
+              onClick={this.goToDanMain}
+            >
               로그인
             </button>
           </form>
