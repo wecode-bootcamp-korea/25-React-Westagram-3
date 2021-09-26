6d594000ec8f19c0241e7bbf7ac43494ebc2ae74
kangdan <kangdanne@gmail.com>
2021-09-26 18:59:55 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/dan/Login/Login.js
+++ src/pages/dan/Login/Login.js
@@ -3,6 +3,29 @@ import { withRouter } from 'react-router-dom';
 import './Login.scss';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      idValue: '',
+      pwValue: '',
+    };
+  }
+
+  //ID state
+  handleIdInput = event => {
+    this.setState({
+      idValue: event.target.value,
+    });
+  };
+
+  // PW state
+  handlePwInput = event => {
+    this.setState({
+      pwValue: event.target.value,
+    });
+  };
+
+  // Main페이지로 이동
   goToDanMain = () => {
     this.props.history.push('/Main-D');
   };
@@ -18,8 +41,16 @@ class Login extends React.Component {
               id="userId"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
+              value={this.state.idValue}
+              onChange={this.handleIdInput}
             />
-            <input id="userPw" type="password" placeholder="비밀번호" />
+            <input
+              id="userPw"
+              type="password"
+              placeholder="비밀번호"
+              value={this.state.pwValue}
+              onChange={this.handlePwInput}
+            />
             <button id="btnLogin" type="submit" onClick={this.goToDanMain}>
               로그인
             </button>
